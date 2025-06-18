842fe00d6606b56c94ee5485fd75b04b8102adad
Dauren <d.k.nurgaziev@gmail.com>
2025-06-18 15:47:42 +0500

    wow animate

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,4 @@
-const allSections = document.querySelectorAll(".features");
-
-let observer = new IntersectionObserver((callback, options) => {
-    callback.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("is-visible");
-            console.log("word")
-            observer.unobserve(entry.target);
-        }
-    });
-}, {
-    threshold: 0.7
-})
-
-allSections.forEach(element => {
-    observer.observe(element);
-});
+document.querySelector(".price").onclick = (event) => {
+    event.preventDefault();
+    document.querySelector(".plans").scrollIntoView({ behavior: "smooth" });
+}
