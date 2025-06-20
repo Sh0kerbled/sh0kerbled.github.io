258c81b835030e5cfe12a11c329c67b04d8c5da7
Dauken Nurgaziyev <d.nurgaziyev@mycar.kz>
2025-06-20 14:19:38 +0500

    everything translated

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,3 @@ document.querySelector(".price").onclick = (event) => {
     event.preventDefault();
     document.querySelector(".plans").scrollIntoView({ behavior: "smooth" });
 }
-
-const lang = navigator.language || navigator.userLanguage;
-if (lang.startsWith('ru')) {
-    window.location.replace('/ru');
-} else {
-    window.location.replace('/en');
-}
