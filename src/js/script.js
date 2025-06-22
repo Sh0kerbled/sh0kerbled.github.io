1ae74787d6dc8e032f86c918e8b6ddb5e6703aa7
Dauren <d.k.nurgaziev@gmail.com>
2025-06-22 14:34:51 +0500

    image link

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,11 @@ document.querySelector(".price").onclick = (event) => {
     event.preventDefault();
     document.querySelector(".plans").scrollIntoView({ behavior: "smooth" });
 }
+
+for (let i = 1; i < 20; i++) {
+    let row = "*";
+    for (let j = 1; j < i; j++) {
+        row += "*";
+    }
+    console.log(row);
+}
